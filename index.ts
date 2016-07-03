5f6c933da0bc637a55f2f76eb53aa7fa0cf14352
Trevor Baron <trevorjbaron@gmail.com>
2016-07-03 00:37:02 -0700

    blue effect

--- index.ts
+++ index.ts
@@ -1,37 +1,47 @@
-"use strict";
+"use strict"
 navigator.getUserMedia = navigator.getUserMedia || navigator["webkitGetUserMedia"] || navigator["mozGetUserMedia"] || navigator["msGetUserMedia"] || navigator["oGetUserMedia"]
 
 class CaptureSession {
   videoElement:any
+  canvasElement:any
+  canvasContext:any
   stream:any
   videoDevice:any
+
+  readFrame(){
+    this.canvasContext.drawImage(this.videoElement, 0, 0, this.videoElement.videoWidth, this.videoElement.videoHeight)
+    return this.canvasContext.getImageData(0, 0, this.videoElement.videoWidth, this.videoElement.videoHeight)
+  }
 
   static async create(videoDevice:any){
     return new Promise<CaptureSession>((resolve, reject) => {
       var ret = new CaptureSession()
 
+      ret.videoDevice = videoDevice
+
       ret.videoElement = document.createElement("video")
       ret.videoElement.autoplay = true
-      //ret.videoElement.width = 300;
-      //ret.videoElement.height = 300;
-      //ret.videoElement.style.width="300px"
-      //ret.videoElement.style.height="100px"
-      //document.body.appendChild(ret.videoElement)
 
+      ret.canvasElement = document.createElement("canvas")
+
+      //FUCK THIS CODE BLOCK
       var videoConstraints:any = {mandatory:{sourceId: videoDevice.deviceId}}
-      var stream = navigator.getUserMedia({video: videoConstraints}, (stream)=>{
-        //var track = stream.getVideoTracks()
-        ret.stream = stream
-        ret.videoElement["src"] = window.URL.createObjectURL(stream)
-        ret.videoElement.addEventListener("play", ()=>{
-          //console.log(ret.videoElement.videoWidth)
-          resolve(ret)
-        })
-
-      }, (e)=>{
-        reject(e)
-      })
-
+      var stream = navigator.getUserMedia(
+        {video: videoConstraints},
+        function onSuccess(stream){
+          ret.stream = stream
+          ret.videoElement["src"] = window.URL.createObjectURL(stream)
+          ret.videoElement.addEventListener("play", ()=>{
+            ret.canvasElement.width = ret.videoElement.videoWidth
+            ret.canvasElement.height = ret.videoElement.videoHeight
+            ret.canvasContext = ret.canvasElement.getContext("2d")
+            resolve(ret)
+          })
+        },
+        function onError(e){
+          reject(e)
+        }
+      )
     })
   }
 }
@@ -46,21 +56,25 @@ var DeviceManager = {
 var runEver
 
 async function main(){
+  //setup camera
   var selectedCamera = (await DeviceManager.findAllVideoDevices())[0]
   var session = await CaptureSession.create(selectedCamera)
 
+  //create canvas
   var canvas:any = document.getElementById('canvas')
+  canvas.width = session.videoElement.videoWidth
+  canvas.height = session.videoElement.videoHeight
   var context = canvas.getContext("2d")
-  var draw = ()=>{
-    //console.log(session.videoElement.videoWidth)
-    try{
-      canvas.width = session.videoElement.videoWidth
-      canvas.height = session.videoElement.videoHeight
 
-      context.drawImage(session.videoElement, 0, 0, session.videoElement.videoWidth, session.videoElement.videoHeight);
-    }catch(e){
-      console.log(e)
+  //render to screen
+  var draw = ()=>{
+    var frame = session.readFrame()
+    var data = frame.data;
+    for (var i = 0, j = 0; j < data.length; i++, j += 4) {
+        data[j + 2] = 255
     }
+
+    context.putImageData(frame, 0, 0);
     requestAnimationFrame(draw)
   }
   draw()
